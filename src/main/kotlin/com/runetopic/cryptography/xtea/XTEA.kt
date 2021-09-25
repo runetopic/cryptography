e8c9988af5334496c9fdaa7d827843a3b3a5cade
Jordan <personal.jordanabraham@gmail.com>
2021-09-24 20:35:00 -0400

    Add XTEA ByteArray extensions and other improvements.

--- src/main/kotlin/com/runetopic/cryptography/xtea/XTEA.kt
+++ src/main/kotlin/com/runetopic/cryptography/xtea/XTEA.kt
@@ -1,55 +1,50 @@
 package com.runetopic.cryptography.xtea
 
-import java.nio.ByteBuffer
+import com.runetopic.cryptography.ext.g4
+import com.runetopic.cryptography.ext.p4
 
 /**
  * @author Jordan Abraham
  */
-class XTEA(
+internal class XTEA(
     private val rounds: Int,
     private val keys: IntArray = IntArray(4)
 ): IXTEA {
     override fun getRounds(): Int = rounds
     override fun getKeys(): IntArray = keys
 
-    override fun decrypt(src: ByteArray): ByteArray {
+    override fun decrypt(src: ByteArray) {
         val amount = Int.SIZE_BYTES * 2
         if (src.size % amount != 0) throw IllegalArgumentException("The src array size must be a multiple of $amount")
-        val buffer = ByteBuffer.wrap(src)
-        val decrypted = ByteBuffer.allocate(src.size)
-        (0 until src.size / amount).forEach { _ ->
-            var v0 = buffer.int
-            var v1 = buffer.int
+        (src.indices step amount).forEach {
+            var v0 = src.g4(0 + it)
+            var v1 = src.g4(4 + it)
             var sum = (DELTA * rounds)
             (0 until rounds).forEach { _ ->
                 v1 -= (v0 shl 4 xor (v0 ushr 5)) + v0 xor sum + keys[sum ushr 11 and 3]
                 sum -= DELTA
                 v0 -= (v1 shl 4 xor (v1 ushr 5)) + v1 xor sum + keys[sum and 3]
             }
-            decrypted.putInt(v0)
-            decrypted.putInt(v1)
+            src.p4(0 + it, v0)
+            src.p4(4 + it, v1)
         }
-        return decrypted.put(buffer).array()
     }
 
-    override fun encrypt(src: ByteArray): ByteArray {
+    override fun encrypt(src: ByteArray) {
         val amount = Int.SIZE_BYTES * 2
         if (src.size % amount != 0) throw IllegalArgumentException("The src array size must be a multiple of $amount")
-        val buffer = ByteBuffer.wrap(src)
-        val encrypted = ByteBuffer.allocate(src.size)
-        (src.indices step amount).forEach { _ ->
-            var v0 = buffer.int
-            var v1 = buffer.int
+        (src.indices step amount).forEach {
+            var v0 = src.g4(0 + it)
+            var v1 = src.g4(4 + it)
             var sum = 0
             (0 until rounds).forEach { _ ->
                 v0 += (v1 shl 4 xor (v1 ushr 5)) + v1 xor sum + keys[sum and 3]
                 sum += DELTA
                 v1 += (v0 shl 4 xor (v0 ushr 5)) + v0 xor sum + keys[sum ushr 11 and 3]
             }
-            encrypted.putInt(v0)
-            encrypted.putInt(v1)
+            src.p4(0 + it, v0)
+            src.p4(4 + it, v1)
         }
-        return encrypted.put(buffer).array()
     }
 
     internal companion object {
